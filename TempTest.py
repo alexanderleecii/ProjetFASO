a7c714d4452e126ba2e4a45d43a91e01d4e9d134
MatthewHaas19 <44777911+MatthewHaas19@users.noreply.github.com>
2018-12-09 21:22:59 +0100

    Update TempTest.py

--- TempTest.py
+++ TempTest.py
@@ -3,7 +3,14 @@ import smbus
 bus = smbus.SMBus(1)
 addr= 0x02
 obj= 0x07
+obj2=0x27
 v=bus.read_byte_data(addr,obj) 
-w=bus.read_byte(obj) 
+w=bus.read_byte(obj)
+x=bus.read_byte_data(addr,obj2) 
+y=bus.read_byte(obj2)
+z=bus.read_byte(addr)
 print v
 print w
+print x
+print y
+print z
